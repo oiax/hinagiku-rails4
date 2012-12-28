ebf826a74aa52b6aeeab79a5f1417b6087b2b9d7
Tsutomu Kuroda <t-kuroda@oiax.jp>
2012-12-28 22:49:01 +0900

    Rails 4.0.0 (dev) skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hinagiku::Application.config.secret_key_base = 'd4fb77747b84f3452ca4067f8f7dfa244cfe360fe817d06e1bb654ca9d0f75ec4617151839a8ba9033c5733feb6246c1a4b44f1544703f555ffa51e0febfe867'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hinagiku::Application.config.session_store :encrypted_cookie_store, key: '_hinagiku_session'
